ccd6109426e83dcaaf1e78c987d55fa819ab862c
gomesuit <gomesuit@gmail.com>
2016-01-11 16:55:13 +0900

    core 0.1.4

--- src/main/java/jailer/core/model/PropertyContents.java
+++ src/main/java/jailer/core/model/PropertyContents.java
@@ -29,4 +29,32 @@ public class PropertyContents {
 	public String toString() {
 		return "PropertyContents [value=" + value + ", hide=" + hide + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (hide ? 1231 : 1237);
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PropertyContents other = (PropertyContents) obj;
+		if (hide != other.hide)
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
 }
